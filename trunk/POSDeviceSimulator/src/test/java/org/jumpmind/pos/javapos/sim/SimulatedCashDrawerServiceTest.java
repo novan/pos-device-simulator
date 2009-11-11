0edc73dbdd3b935dbfe7892067e8e550a285b93d
josh.a.hicks <josh.a.hicks@e8a602fc-ceea-11de-9e88-ff79fe1e0c1e>
2009-11-11 21:37:44 +0000

    Added unit test for CashDrawerSimulatedService

---
+++ trunk/POSDeviceSimulator/src/test/java/org/jumpmind/pos/javapos/sim/SimulatedCashDrawerServiceTest.java
@@ -0,0 +1,43 @@
+package org.jumpmind.pos.javapos.sim;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SimulatedCashDrawerServiceTest {
+	SimulatedCashDrawerService service;
+	
+	@Before
+	public void before() {
+		service = new SimulatedCashDrawerService();
+	}
+	
+	@Test
+	public void testReset() throws Exception {
+		service.openDrawer();
+		service.reset();
+		Assert.assertFalse(service.getDrawerOpened());
+	}
+
+	@Test
+	public void testOpenDrawer() throws Exception {
+		service.openDrawer();
+		Assert.assertTrue(service.getDrawerOpened());
+	}
+	
+	@Test
+	public void testToggleDrawerOpen() throws Exception {
+		service.toggleDrawer(true);
+		Assert.assertTrue(service.getDrawerOpened());
+	
+		service.toggleDrawer(true);
+		Assert.assertTrue(service.getDrawerOpened());
+	}
+	
+	@Test
+	public void testToggleDrawerClose() throws Exception {
+		service.toggleDrawer(false);
+		Assert.assertFalse(service.getDrawerOpened());
+	}
+	
+}
